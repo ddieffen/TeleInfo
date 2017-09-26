b5109b671ac14fc4dc78c7d94a070abb07cab787
David Dieffenthaler <ddieffen@gmail.com>
2017-09-26 20:22:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+   <head>
+      <title>Consommation</title>
+      <meta http-equiv="refresh" content="60" >
+   </head>
+   <body>
+      <?php
+        include('teleinfo_graph.php')
+      ?>
+   </body>
+</html>
