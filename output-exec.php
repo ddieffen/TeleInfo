5ec863afb3933ff1cfb1c16170646c8964e91474
David Dieffenthaler <ddieffen@gmail.com>
2018-03-18 18:52:35 +0100

    Added files

--- output-exec.php
+++ output-exec.php
@@ -1,12 +1,18 @@
 <?php
 
 // Lit un fichier, et le place dans une chaîne
+$filename = "/var/www/html/mode-vac";
+$handle = fopen($filename, "r");
+$modevac = fread($handle, filesize($filename));
+fclose($handle);
+
+// Lit un fichier, et le place dans une chaîne
 $filename = "/var/www/html/heat-salon";
 $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
 
-if($contents == "ON"){
+if($contents == "ON" && $modevac == "OFF"){
   $gpio_off = shell_exec("sudo gpio write 0 1");
   echo("checked=checked");
 }
@@ -21,7 +27,7 @@ $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
 
-if($contents == "ON"){
+if($contents == "ON" && $modevac == "OFF"){
   $gpio_off = shell_exec("sudo gpio write 2 1");
   echo("checked=checked");
 }
@@ -36,7 +42,7 @@ $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
 
-if($contents == "ON"){
+if($contents == "ON" && $modevac == "OFF"){
   $gpio_off = shell_exec("sudo gpio write 3 1");
   echo("checked=checked");
 }
