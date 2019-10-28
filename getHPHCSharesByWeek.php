6cd17b487240c5aa6b472550464b2b32a6cdbb91
David Dieffenthaler <ddieffen@gmail.com>
2019-10-28 01:40:41 +0100

    correction weekly data

--- getHPHCSharesByWeek.php
+++ getHPHCSharesByWeek.php
@@ -8,7 +8,7 @@ $sqlite = '/home/dietpi/teleinfo.sqlite';
   function getHPHCSharesByWeek () {
     global $sqlite;
     $db = new SQLite3($sqlite);
-    $results = $db->query("select strftime('%Y-%W', datetime(timestamp, 'unixepoch', 'localtime')) as week, 100*sum(daily_hc)/(sum(daily_hc)+sum(daily_hp)) as shareHC, 100*sum(daily_hp)/(sum(daily_hc)+sum(daily_hp)) as shareHP from conso group by week;");
+    $results = $db->query("select strftime('%Y-%W', datetime(timestamp-1, 'unixepoch', 'localtime')) as week, 100*sum(daily_hc)/(sum(daily_hc)+sum(daily_hp)) as shareHC, 100*sum(daily_hp)/(sum(daily_hc)+sum(daily_hp)) as shareHP from conso group by week;");
 
     $data = array();
 
